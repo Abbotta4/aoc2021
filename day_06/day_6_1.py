ea7071377f4f5b53df5dd55acecb9938c012009c
Drew Abbott <abbotta4@gmail.com>
2021-12-06 02:37:00 -0600

    Complete Day 6 part 1

---
+++ day_06/day_6_1.py
@@ -0,0 +1,15 @@
+with open("input.txt", 'r') as f:
+    input_fish = f.readline().split(',')
+input_fish = list(map(int, input_fish))
+fish = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+for f in input_fish:
+    fish[f] += 1
+
+for day in range(80):
+    new_fish = fish[0]
+    for i in range(8):
+        fish[i] = fish[i+1]
+    fish[8] = new_fish # actual new fish
+    fish[6] += new_fish # freshly spawned
+
+print(sum(fish))
